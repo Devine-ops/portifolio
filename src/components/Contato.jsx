da8f96ae1c5b340b659c5573380a0e42400ba53c
Vinicius Gabriel <viniganrielsoa@gmail.com>
2025-01-12 15:27:29 -0300

    :deploy:

--- src/components/Contato.jsx
+++ src/components/Contato.jsx
@@ -4,20 +4,23 @@ import { FaGithub, FaWhatsapp, FaSquareGooglePlus, FaLinkedin  } from "react-ico
 function Contato() {
     return (
         <div className={styles.container}>
-        <div className={styles.laptop}>
-            <div className={styles.screen}>
-                <div className={styles.header}></div>
-                <div className={styles.text}>entre em contato!</div>
-                
-            </div>
-            <div className={styles.keyboard}></div>
-        </div>
-        <div className={styles.contacts}>
-            <a href="https://github.com/Devine-ops"><FaGithub /></a>
-            <a href="https://wa.link/g6wosm"><FaWhatsapp /></a>
-            <a href="https://mail.google.com/mail/u/2/#inbox?compose=DmwnWrRnXfFKspgCxMDDKmqKlHBnkjLkkjwCpkGdlVgVVmflrlcWjfKszwSjgbptWFzwgmScnPBg"><FaSquareGooglePlus /></a>
-            <a href="www.linkedin.com/in/vinicius-gabriel-oliveira-de-sousa"><FaLinkedin /></a>
-        </div>
+            
+                <div className={styles.laptop}>
+                    <div className={styles.screen}>
+                        <div className={styles.header}></div>
+                        <div className={styles.text}>entre em contato!</div>
+                        
+                    </div>
+                    <div className={styles.keyboard}></div>
+                </div>
+                <div className={styles.wrap}>
+                    <div className={styles.contacts}>
+                        <a href="https://github.com/Devine-ops"><FaGithub /></a>
+                        <a href="https://wa.link/g6wosm"><FaWhatsapp /></a>
+                        <a href="https://mail.google.com/mail/u/2/#inbox?compose=DmwnWrRnXfFKspgCxMDDKmqKlHBnkjLkkjwCpkGdlVgVVmflrlcWjfKszwSjgbptWFzwgmScnPBg"><FaSquareGooglePlus /></a>
+                        <a href="www.linkedin.com/in/vinicius-gabriel-oliveira-de-sousa"><FaLinkedin /></a>
+                    </div>
+                </div>
         </div>
     )
 }
